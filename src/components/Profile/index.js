ea322dbd7ecafadb03e1e38cd6b19f9d08be4f0d
Stefany <stefany.vasc.sa@gmail.com>
2020-11-14 03:34:05 -0300

    ✨ create component Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,37 @@
+import { graphql, useStaticQuery } from "gatsby"
+import React from "react"
+import Avatar from "../Avatar"
+import * as S from "./styled"
+
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { title, position, description },
+    },
+  } = useStaticQuery(graphql`
+    query MySiteMetadata {
+      site {
+        siteMetadata {
+          title
+          description
+          position
+        }
+      }
+    }
+  `)
+
+  return (
+    <S.ProfileWrapper>
+      <Avatar />
+      <S.ProfileLink>
+        <S.ProfileAuthor>
+          {title}
+          <S.ProfilePosition>{position}</S.ProfilePosition>
+        </S.ProfileAuthor>
+      </S.ProfileLink>
+      <S.ProfileDescription>{description}</S.ProfileDescription>
+    </S.ProfileWrapper>
+  )
+}
+
+export default Profile
