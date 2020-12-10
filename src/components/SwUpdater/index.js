5ce2e6900f6e7cab59b95872957589769bb3d8d9
Stefany <stefany.vasc.sa@gmail.com>
2020-12-10 19:58:20 -0300

    ✨ create component SwUpdater

---
+++ src/components/SwUpdater/index.js
@@ -0,0 +1,14 @@
+import { Refresh } from "@styled-icons/boxicons-regular/Refresh"
+import React from "react"
+import * as S from "./styled"
+
+const SwUpdater = () => (
+  <S.NotificationWrapper>
+    <p>Tem uma atualização disponível.</p>
+    <S.IconWrapper>
+      <Refresh onClick={() => window.location.reload()} />
+    </S.IconWrapper>
+  </S.NotificationWrapper>
+)
+
+export default SwUpdater
