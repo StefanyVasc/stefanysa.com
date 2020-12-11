ad00d8fc0782c90eb66091532c143b18976ecf83
Stefany <stefany.vasc.sa@gmail.com>
2020-12-10 22:14:39 -0300

    🔥 remove wrong  cache plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,11 +94,5 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify-cms`,
-    {
-      resolve: "gatsby-plugin-netlify-cache",
-      options: {
-        cachePublic: true,
-      },
-    },
   ],
 }
